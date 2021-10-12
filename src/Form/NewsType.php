20c8e599f1c32c3855aae042ebc2e7b32666382f
Arthur Isnard <artisnard@gmail.com>
2021-10-12 09:39:01 +0200

    feat: form styling + auth

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -6,7 +6,9 @@ use App\Entity\News;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class NewsType extends AbstractType
 {
@@ -16,7 +18,9 @@ class NewsType extends AbstractType
             ->add('name')
             ->add('body')
             ->add('short_desc')
-            ->add('save', SubmitType::class)
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'px-5 py-2 bg-gray-300 rounded-md shadow-xl hover:bg-gray-400'],
+            ])
         ;
     }
 
